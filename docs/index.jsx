29c52a6f6d4ed75f3e9f242c07d14a7618e08d3b
yangzuhao <yangzuhao@zuoyebang.com>
2020-08-17 21:07:54 +0800

    首页构建

--- docs/index.jsx
+++ docs/index.jsx
@@ -1,1 +1,1 @@
-<iframe allowtransparency="yes" frameborder="0" width="100%" height="100%" src="https://flyaha.top/page/"/>
+<iframe allowtransparency="yes" frameborder="0" width="100%" height="100%" src="http://flyaha.top/page/"/>
